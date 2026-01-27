fa96ad9a39fb86c3751aa054c3a9a412ca9d5181
Hideri <derana765@gmail.com>
2026-01-27 14:00:25 +0700

    Refactor database seeder to use dedicated seeders for permissions, users, and settings; implement UserController with basic index method; create Module, Role, and Setting models; add policies for Role, Setting, and User; implement UserService for user retrieval; create migrations for modules, settings, and permissions; seed initial permissions and settings data; add user seeder for predefined roles and users.

---
+++ app/Policies/SettingPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Setting;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SettingPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user): bool
+    {
+        return $user->can('setting.index');
+    }
+
+    public function view(User $user, Setting $setting): bool
+    {
+        return $user->can('setting.index');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->can('setting.create');
+    }
+
+    public function update(User $user, Setting $setting): bool
+    {
+        return $user->can('setting.edit');
+    }
+
+    public function delete(User $user, Setting $setting): bool
+    {
+        return false;
+    }
+}
